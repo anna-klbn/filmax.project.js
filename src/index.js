36b69c6957a29c8f1bbf9b91c785f6581847d11b
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2023-12-20 20:03:21 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -5,37 +5,3 @@ import './js/input-search.js';
 import "./js/header-menu.js";
 import "./js/swiper.js";
 import "./js/movie.js";
-
-
-
-// function markupMovies(data) {
-//   document.querySelector(".movies").innerHTML = "";
-
-//     data.items.map((item) => {
-//         const movieEl = document.createElement("div");
-//         movieEl.classList.add("movie");
-//         movieEl.innerHTML = `<div class="movie__cover-inner">
-//              <img
-//                src="${item.posterUrl}"
-//                class="movie__cover"
-//                alt="${item.nameRu}"
-//              />
-//              <div class="movie__cover--darkenet"></div>
-//           </div>
-//            <ul class="movie__info">
-//              <li class="movie__title">${item.nameRu}</li>
-//              <li class="movie__category">${item.genres.map(genre => `${genre.genre}`)}</li></ul>
-//              <div class="movie__rating">${item.ratingKinopoisk}</div>
-//            </div`;
-//       moviesEl.appendChild(movieEl);
-//     });
-// }
-// function showMovies() {
-//   const movies = getMovies(API_URL).then(movies => markupMovies(movies));
-  
-// }
-
-
-
-
-
--- src/js/movie.js
+++ src/js/movie.js
@@ -1,17 +1,153 @@
-// const API_KEY = "a80bb377f6b7e60d939d42f267fc679a";
-// const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = "a80bb377f6b7e60d939d42f267fc679a";
+const BASE_URL = "https://api.themoviedb.org/3";
+const IMAGE_URL = "https://image.tmdb.org/t/p/w300";
 
-// const API_URL = `https://api.themoviedb.org/3/movie/550?api_key=a80bb377f6b7e60d939d42f267fc679a`;
+const form = document.querySelector('.form');
 
-// getMovies(API_URL);
+// Популярні фільми
+// fetch(`${BASE_URL}/movie/popular?language=en-US&page=1&api_key=${API_KEY}`)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//     .catch(err => console.error(err));
+  
 
-// function getMovies(url) {
-//     fetch(url)
-//   .then(response => 
-//      response.json()
-//   )
-//   .then(data => {
-//     console.log(data);
-//   });
-// }
+// // Рейтингові фільми
+// fetch(`${BASE_URL}/movie/top_rated?language=en-US&page=1&api_key=${API_KEY}`)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//     .catch(err => console.error(err));
+  
+// // Що очікуються
+// fetch(`${BASE_URL}/movie/upcoming?language=en-US&page=1&api_key=${API_KEY}`)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//     .catch(err => console.error(err));
+  
+// // Пошук за назвою
+// fetch(`${BASE_URL}/search/movie?include_adult=false&language=en-US&page=1&api_key=${API_KEY}`)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//     .catch(err => console.error(err));
 
+const moviesContainer = document.querySelector('.movies-container');
+const topContainer = document.querySelector('.top-container');
+const upcomingContainer = document.querySelector('.upcoming-container');
+  
+
+getPopularMovies(`${BASE_URL}/movie/popular?language=en-US&page=1&api_key=${API_KEY}`);
+
+function getPopularMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        showMovies(data.results)
+    })
+}
+
+function showMovies(data) {
+    
+    moviesContainer.innerHTML = ""
+    data.forEach(movie => { 
+        const { title, poster_path, vote_average, release_date} = movie;
+        const movieEl = document.createElement("div");
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `<img
+            src="${IMAGE_URL+poster_path}"
+            alt="${title}"
+            class="movie-img"
+          />
+          <div class="movie-info">
+            <h3 class="movie-title">
+             ${title}
+              <span class="${getColor(vote_average)}">${vote_average.toFixed(1)}</span>
+            </h3>
+            <h3 class="movie-data">${release_date}</h3>`
+        moviesContainer.appendChild(movieEl);
+        
+    })
+};
+
+function getColor(vote) {
+    if (vote >= 8) {
+        return "green"
+    } else if (vote >= 5) {
+        return "orange"
+    } else {
+        return "red"
+    }
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = e.currentTarget.searchQuery.value;
+    console.log(value)
+    if (value) {
+        getPopularMovies(`${BASE_URL}/search/movie?include_adult=false&language=en-US&page=1&api_key=${API_KEY}`)
+    }
+    //  else {
+    //     getPopularMovies(`${BASE_URL}/movie/popular?language=en-US&page=1&api_key=${API_KEY}`)
+    // }
+})
+
+
+
+getTopMovies(`${BASE_URL}/movie/top_rated?language=en-US&page=1&api_key=${API_KEY}`);
+
+function getTopMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        showTopMovies(data.results)
+    })
+}
+
+function showTopMovies(data) {
+    
+    topContainer.innerHTML = ""
+    data.forEach(movie => { 
+        const { title, poster_path, vote_average, release_date} = movie;
+        const movieEl = document.createElement("div");
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `<img
+            src="${IMAGE_URL+poster_path}"
+            alt="${title}"
+            class="movie-img"
+          />
+          <div class="movie-info">
+            <h3 class="movie-title">
+             ${title}
+              <span class="${getColor(vote_average)}">${vote_average.toFixed(1)}</span>
+            </h3>
+            <h3 class="movie-data">${release_date}</h3>`
+        topContainer.appendChild(movieEl);
+        
+    })
+};
+
+
+
+getUpcomingMovies(`${BASE_URL}/movie/upcoming?language=en-US&page=1&api_key=${API_KEY}`);
+
+function getUpcomingMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        showUpcomingMovies(data.results)
+    })
+}
+
+function showUpcomingMovies(data) {
+    
+    upcomingContainer.innerHTML = ""
+    data.forEach(movie => { 
+        const { title, poster_path, vote_average, release_date} = movie;
+        const movieEl = document.createElement("div");
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `<img
+            src="${IMAGE_URL+poster_path}"
+            alt="${title}"
+            class="movie-img"
+          />
+          <div class="movie-info">
+            <h3 class="movie-title">
+             ${title}
+              <span class="${getColor(vote_average)}">${vote_average.toFixed(1)}</span>
+            </h3>
+            <h3 class="movie-data">${release_date}</h3>`
+        upcomingContainer.appendChild(movieEl);
+        
+    })
+};
